3cdb077cbe06f77141e3b178a58b900d374bfb84
Ayush_Mann <learnertsk@gmail.com>
2020-07-01 22:32:40 +0530

    minor changes in the destroy and update and show actions in users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+    email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+    user: { password:
+    "password",
+    password_confirmation: "password",
+    admin: false } }
+    assert_not @other_user.admin?
+  end
+
 end
